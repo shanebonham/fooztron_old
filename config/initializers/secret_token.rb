94bb7c44dcd8674b452898d9718f336b3e5633c1
Etienne de Bruin <etdebruin@gmail.com>
2012-04-25 18:50:14 -0500

    rails noo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fooztron::Application.config.secret_token = '9bae31153d46d4fda481aea1364a9ddc7ba26019878d66460f2fe2ee0187e45d497c00722f28d01f878a8f46c77ad6e8cf32ec551968e3bf87fc4d3966fc2f9b'
