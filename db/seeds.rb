fb6129feefdbdc4a6fae01c2467c76eb2f2c3808
Shane Bonham <shanebonham@gmail.com>
2012-04-25 23:30:18 -0500

    Add scores to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 players = Player.create([{ name: 'Shane' }, { name: 'Etienne' }, { name: 'Beau' }, { name: 'Benson' }])
-Game.create(white_offense_id: players[1],
-            white_defense_id: players[3],
-            blue_offense_id: players[2],
-            blue_defense_id: players[0]
+Game.create(white_offense_id: players[1].id,
+            white_defense_id: players[3].id,
+            blue_offense_id: players[2].id,
+            blue_defense_id: players[0].id,
+            white_score: 1,
+            blue_score: 10
             )
