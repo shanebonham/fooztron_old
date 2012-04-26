19dc33bfdca9c66abe2834451f99775437007a4b
Shane Bonham <shanebonham@gmail.com>
2012-04-25 22:57:11 -0500

    Add dropdowns for player names and game scores

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,6 +25,7 @@ class GamesController < ApplicationController
   # GET /games/new.json
   def new
     @game = Game.new
+    @players = Player.all
 
     respond_to do |format|
       format.html # new.html.erb
