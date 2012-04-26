447a5e76c4d0983bb0981a810dd89f085de42de7
Shane Bonham <shanebonham@gmail.com>
2012-04-25 22:57:38 -0500

    Make game attributes accessible

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,5 @@ class Game < ActiveRecord::Base
   belongs_to :blue_offense, :class_name => 'Player'
   belongs_to :blue_defense, :class_name => 'Player'
 
+  attr_accessible :white_offense_id, :white_defense_id, :blue_offense_id, :blue_defense_id, :white_score, :blue_score
 end
