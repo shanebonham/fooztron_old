94c4f9cc95f0418cd14960d23d62a921ee634d1e
Shane Bonham <shanebonham@gmail.com>
2012-05-18 20:29:26 -0700

    Delete associated played positions when games are deleted. Fix #11

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ActiveRecord::Base
-  has_many :played_positions
+  has_many :played_positions, :dependent => :destroy
   has_many :players, :through => :played_positions
 
   attr_accessible :white_score, :blue_score, :played_positions_attributes
