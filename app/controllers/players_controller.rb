a50f0f3bb62c7d795dd4b9ac21aa270f2572bd01
Shane Bonham <shanebonham@gmail.com>
2012-05-30 08:31:48 -0700

    Improve leaderboard so that it's limited to at least one game by default. #21

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -83,16 +83,14 @@ class PlayersController < ApplicationController
 
   def leaderboard
     @players = [];
+    @number_of_games = 1
     if params[:number].present?
-      number_of_games = params[:number].to_i
-      Player.all.each do |player|
-        if player.total_games_played >= number_of_games
-          @players << player
-        end
-      end
-    else
-      @players = Player.all
+      @number_of_games = params[:number].to_i
     end
-    @players
+    Player.all.each do |player|
+      if player.total_games_played >= @number_of_games
+        @players << player
+      end
+    end
   end
 end
